f5a33362ffdbf5fedc70ffa54efa9f4df37b1ca7
hakeem <abdhakeem123abd@gmail.com>
2026-09-06 16:16:24 +0530

    Fix inventory image saving

--- api/models.py
+++ api/models.py
@@ -30,6 +30,7 @@ class InventoryItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)
     is_available = models.BooleanField(default=True)
+    img = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
